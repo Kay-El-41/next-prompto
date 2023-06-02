edee4d527ae3b94471f7ae155fb92816d9ee2e74
Kay-El-41 <z.khantolino@gmail.com>
2023-06-02 11:05:45 +0630

    finish

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,21 +3,28 @@
 import { useState, useEffect } from 'react'
 import PromptCard from './PromptCard'
 
-
-const PromptCardList = ({data, handleTagClick}) => {
-  return <div className='mt-16 prompt_layout'>
-    {data.map((post) => (
-      <PromptCard key={post._id} post={post}
-      handleTagClick = {handleTagClick}/>
-    ))}
-  </div>
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
 }
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('')
   const [posts, setPosts] = useState([])
 
-  useEffect(()=>{
+  const [searchText, setSearchText] = useState('')
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch('/api/prompt')
       const data = await response.json()
@@ -25,16 +32,35 @@ const Feed = () => {
       setPosts(data)
     }
     fetchPosts()
-  },[])
+  }, [])
 
+  const filterPrompts = (searchTerm) => {
+    const regex = new RegExp(searchTerm, 'i') // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
 
   const handleChange = (e) => {
     e.preventDefault()
+    clearTimeout(searchTimeout)
     setSearchText(e.target.value)
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value)
+        setSearchResults(searchResult)
+      }, 500)
+    )
   }
 
-  const handleTagClick = () => {
-
+  const handleTagClick = (tag) => {
+    setSearchText(tag)
+    const searchResult = filterPrompts(tag)
+    setSearchResults(searchResult)
   }
 
   return (
@@ -49,8 +75,12 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-    <PromptCardList data={posts} handleTagClick = {handleTagClick}/>
 
+      {searchText ? (
+        <PromptCardList data={searchResults} handleTagClick={handleTagClick} />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -16,10 +16,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(''), 3000)
   }
 
+  const handleProfileClick = () => {
+    if (post.creator._id === session?.user.id) return router.push('/profile')
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
+  }
+
   return (
     <div className="prompt_card">
       <div className=" flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -46,6 +54,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             }
             width={12}
             height={12}
+            alt="copy"
           />
         </div>
       </div>
@@ -54,14 +63,14 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm green_gradient cursor-pointer"
-            onClick={handleEdit}
+            onClick={() => handleEdit(post.tag)}
           >
             Edit
           </p>
